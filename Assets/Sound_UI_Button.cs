f0ed5b40c5999b221f0e91e124f40ecd22305999
AndreyBirchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2020-04-18 13:41:41 +0300

    Добавил звуки меню

---
+++ Assets/Sound_UI_Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_UI_Button : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string UIButton; // Переменная для ивента
+
+    FMOD.Studio.EventInstance EventInstance; // Переменная для инстанаса
+
+    public void PlayUiButton()
+    {
+        EventInstance = FMODUnity.RuntimeManager.CreateInstance(UIButton);//создаёт  контейнер для семпла
+        EventInstance.start();//Проигрывает этот контейнер
+        EventInstance.release();//Удаляет этот контейнер
+    }
+}
