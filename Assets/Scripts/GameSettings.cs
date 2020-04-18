ceb3e89a248cce45bbdc3c1b69424dfb4f695848
Hexkritor <Hexkritor@yandex.com>
2020-04-18 07:57:24 +0700

    Added Jam Template
    Добавлен небольшой шаблон для джема: меню, настройки звука и выход из приложения

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    //linkage
+    public GameObject settingsPopup;
+    //varrialbes
+    private GameObject _settingsPopup;
+
+
+    void Update()
+    {
+        if (Input.GetAxis("Cancel") == 1 && !_settingsPopup)
+            _settingsPopup = Instantiate(settingsPopup);
+    }
+}
