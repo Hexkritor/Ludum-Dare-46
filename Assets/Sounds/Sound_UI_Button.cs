caef4e14e4bdaab4819a693106a85a22d3fee1b2
AndreyBirchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2020-04-19 10:32:07 +0300

    Merge branch 'develop' of https://github.com/Hexkritor/Ludum-Dare-46 into develop

---
+++ Assets/Sounds/Sound_UI_Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_UI_Button : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string UI_Button_Sound; // Переменная для ивента
+
+    FMOD.Studio.EventInstance EventInstance; // Переменная для инстанаса
+
+    public void Play_UI_Sound()
+    {
+        EventInstance = FMODUnity.RuntimeManager.CreateInstance(UI_Button_Sound);//создаёт  контейнер для семпла
+        EventInstance.start();//Проигрывает этот контейнер
+        EventInstance.release();//Удаляет этот контейнер
+    }
+}
